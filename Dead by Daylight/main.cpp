2c2f0381f49f8ed3043c021198327e72dd49badb
Jonesジョーンズ <jooj@uef.fi>
2021-06-21 21:07:30 +0300

    uwu

--- Dead by Daylight/main.cpp
+++ Dead by Daylight/main.cpp
@@ -34,11 +34,15 @@ uintptr_t actors;
 uintptr_t gameinstance;
 int actor_count;
 
+char lWindowName[256] = "Overlay";
+char tWindowName[256] = "DeadByDaylight  "; // put Game window name here
+
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 MARGINS pMargin;
 MSG Message;
 HANDLE hnd;
 DWORD id;
+int entityloop();
 int render();
 
 LRESULT CALLBACK Proc(HWND hWnd, UINT Message, WPARAM wParam, LPARAM lParam)
@@ -64,8 +68,181 @@ LRESULT CALLBACK Proc(HWND hWnd, UINT Message, WPARAM wParam, LPARAM lParam)
 	}
 }
 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	AllocConsole();
+	freopen("conin$", "r", stdin);
+	freopen("conout$", "w", stdout);
+	freopen("conout$", "w", stderr);
+	printf("Debugging Window:\n");
 
-int main() {
+	Kernel::hKernelDriver = CreateFileA(("\\\\.\\NEET"), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
+
+	if (Kernel::hKernelDriver == INVALID_HANDLE_VALUE)
+	{
+		std::cout << "Driver not loaded Exit in 2 Secs..." << std::endl;
+		Sleep(2000);
+		exit(0);
+	}
+	//else {
+		//std::cout << _xor_("Driver Loaded! \n") << std::endl;
+	//}
+
+	while (hGameWnd == NULL)
+	{
+		hGameWnd = FindWindow(0, Kernel::WINDOWNAME);
+		Sleep(1000);
+	}
+
+	if (hGameWnd == NULL)
+	{
+		std::cout << "Window %s Not FOUND Exiting......" << std::endl;
+
+		Sleep(1000);
+		exit(0);
+	}
+
+	GetWindowThreadProcessId(hGameWnd, &Kernel::ProcessID);
+
+	info_t Input_Output_Data;
+	Input_Output_Data.pid = Kernel::ProcessID;
+	DWORD Readed_Bytes_Amount;
+
+	DeviceIoControl(Kernel::hKernelDriver, ctl_base, &Input_Output_Data, sizeof Input_Output_Data, &Input_Output_Data, sizeof Input_Output_Data, &Readed_Bytes_Amount, nullptr);
+	Kernel::GameModule = (QWORD)Input_Output_Data.data;
+
+	//DumpEXE(GameModule);
+
+	std::cout << ("ProcessID: ") << Kernel::ProcessID << std::endl;
+	//std::cout << ("GameBase: 0x") << std::uppercase << std::hex << Kernel::GameModule << std::endl;
+	std::cout << "GameBase: " << Kernel::GameModule << std::endl;
+
+	/*
+	HWND MyWindowHwnd = FindWindowA(0, "Apex Legends");
+
+
+	//hnd = CreateThread(NULL, // security attributes ( default if NULL )
+	//	0, // stack SIZE default if 0
+	//	(LPTHREAD_START_ROUTINE)thread, // Start Address
+	//	NULL, // input data
+	//	0, // creational flag ( start if  0 )
+	//	&id); // thread ID
+
+	RECT rect;
+	if (GetClientRect(MyWindowHwnd, &rect)) {
+		clientWidth = rect.right - rect.left;
+		clientHeight = rect.bottom - rect.top;
+	};
+	*/
+	WNDCLASSEXA OverlayWnd; // contains window class information
+	OverlayWnd.cbSize = sizeof(WNDCLASSEXA); // size of struct, basically checking for version or check
+	OverlayWnd.style = CS_HREDRAW | CS_VREDRAW;  // Style, redraw method type
+	OverlayWnd.lpfnWndProc = Proc; // Pointer to the window procedure
+	OverlayWnd.cbClsExtra = 0; // window class struct extra bytes
+	OverlayWnd.cbWndExtra = 0; // window instance extra bytes
+	OverlayWnd.hInstance = hInstance; // handle to the instance that contains the window procedure for the class
+	OverlayWnd.hIcon = LoadIcon(NULL, IDI_APPLICATION); // basic window icon set
+	OverlayWnd.hIconSm = LoadIcon(NULL, IDI_APPLICATION); // basic window icon set
+	OverlayWnd.hCursor = LoadCursor(NULL, IDC_ARROW); // basic window cursor icon set
+	OverlayWnd.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(0, 0, 0)); // handle to the class background brush
+	OverlayWnd.lpszMenuName = lWindowName;
+	OverlayWnd.lpszClassName = lWindowName;
+
+
+	// registers a window class for the use in call to this createwindowex func
+	if (!RegisterClassExA(&OverlayWnd))
+	{
+		::UnregisterClass(OverlayWnd.lpszClassName, OverlayWnd.hInstance);
+
+		exit(1);
+	}
+
+	TargetWnd = FindWindowA(0, tWindowName);
+
+	if (TargetWnd)
+	{
+		GetWindowRect(TargetWnd, &WindowRect);
+		windowWidth = WindowRect.right - WindowRect.left;
+		windowHeight = WindowRect.bottom - WindowRect.top;
+
+		std::cout << "width: " << windowWidth << " " << "height: " << windowHeight << std::endl;
+		pMargin = { 0, 0, windowHeight, windowWidth };
+		hWnd = CreateWindowExA(WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_LAYERED, lWindowName, lWindowName, WS_POPUP, 1, 1, windowWidth, windowHeight, 0, 0, 0, 0);
+	}
+
+	SetLayeredWindowAttributes(hWnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
+	SetLayeredWindowAttributes(hWnd, 0, 255, LWA_ALPHA);
+
+	ShowWindow(hWnd, /*SW_SHOW*/SW_SHOWDEFAULT);
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	(void)io;
+	io.DisplaySize.x = clientWidth/*windowWidth*/;
+	io.DisplaySize.y = clientHeight/*windowHeight*/;
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplWin32_Init(hWnd);
+	D3D9Init(hWnd);
+
+
+	for (;;)
+	{
+
+		if (GetAsyncKeyState(VK_END))
+			break;
+
+
+		if (PeekMessage(&Message, hWnd, 0, 0, PM_REMOVE))
+		{
+
+			TranslateMessage(&Message);
+
+			DispatchMessage(&Message);
+		}
+
+		TargetWnd = FindWindowA(0, tWindowName);
+
+
+		if (!TargetWnd)
+			exit(0);
+
+		GetWindowRect(TargetWnd, &WindowRect);
+		GetClientRect(TargetWnd, &ClientRect);
+
+		windowWidth = WindowRect.right - WindowRect.left;
+		windowHeight = WindowRect.bottom - WindowRect.top;
+
+
+		DWORD dwStyle = GetWindowLong(TargetWnd, GWL_STYLE);
+
+		if (dwStyle & WS_BORDER)
+		{
+			windowHeight = WindowRect.bottom - WindowRect.top;
+			windowWidth = WindowRect.right - WindowRect.left;
+			clientHeight = ClientRect.bottom - ClientRect.top;
+			clientWidth = ClientRect.right - ClientRect.left;
+			borderHeight = (windowHeight - ClientRect.bottom);
+			borderWidth = (windowWidth - ClientRect.right) / 2; //only want one side
+			borderHeight -= borderWidth; //remove bottom from width, bottom is the same size as either side so we subtract that
+
+			WindowRect.left += borderWidth;
+			WindowRect.top += borderHeight;
+		}
+
+		MoveWindow(hWnd, WindowRect.left, WindowRect.top, clientWidth, clientHeight, true);
+
+	} // End of Panic Loop
+
+	ImGui::DestroyContext();
+	exit(0);
+
+	return 0;
+}
+
+
+int entityloop() {
 
 	AllocConsole();
 	freopen("conin$", "r", stdin);
@@ -238,4 +415,67 @@ int main() {
 
 
 	system("pause");
+}
+
+int render() {
+
+	dx_Device->Clear(0, 0, D3DCLEAR_TARGET, 0, 1.0f, 0);
+	dx_Device->BeginScene();
+
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	if (TargetWnd == GetForegroundWindow())
+	{
+		time_t now = time(0);
+		// convert now to string form
+		char* dt = ctime(&now);
+		// convert now to tm struct for UTC
+		//tm *gmtm = gmtime(&now);
+		//dt = asctime(gmtm);
+		char stamp[256] = "Shionji ";
+		strcat(stamp, dt);
+		DrawString(stamp, 10, 10, 255, 0, 255, dx_FontCalibri); // Put Main procedure here like ESP etc.
+
+	}
+
+	//w2s.Update();
+
+
+	if (GetAsyncKeyState(VK_INSERT) & 0x8000) {
+		if (show_menu)
+			show_menu = false;
+		else
+			show_menu = true;
+		Sleep(200);
+	}
+
+	if (!show_menu) {
+		//	Circle((int)(clientWidth / 2), (int)(clientHeight / 2), fov, 0, full, true, 32, BLACK(255));
+		entityloop();
+	}
+
+
+
+	if (show_menu)
+	{
+
+		menu();
+
+	}
+	else if (windowstate != 0)
+	{
+		ChangeClickability(false, ohwnd);
+	}
+	ImGui::EndFrame();
+
+
+	ImGui::Render();
+	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+	dx_Device->EndScene();
+	dx_Device->PresentEx(0, 0, 0, 0, 0);
+
+	return 0;
+
 }
